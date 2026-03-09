fef26a3919769ee42714077adb0f91e1c918677b
gauravbarodia <gaurav.barodia.min22@itbhu.ac.in>
2026-03-09 15:01:38 +0530

    fix dis

--- Dijkstra.hpp
+++ Dijkstra.hpp
@@ -13,8 +13,8 @@ namespace Dijkstra
     std ::vector<int> Seq_Di(int start, int nodes)
     {
 
-        std ::vector<int> dist(nodes,INT_MAX);
-        std ::priority_queue<std ::pair<int, int>,std :: vector<std :: pair<int,int>>, std :: greater<>> pq;
+        std ::vector<int> dist(nodes, INT_MAX);
+        std ::priority_queue<std ::pair<int, int>, std ::vector<std ::pair<int, int>>, std ::greater<>> pq;
         pq.push({0, start});
         dist[start] = 0;
         while (pq.size())
@@ -39,54 +39,71 @@ namespace Dijkstra
         }
         return dist;
     }
-    std ::vector<int> Para_Di(int start, int nodes, Thread_Pool &pool, std ::atomic<int> &_threads)
+    std::vector<int> Para_Di(int start, int nodes, Thread_Pool &pool, std::atomic<int> &_threads)
     {
-        std ::vector<int> dist(nodes, INT_MAX);
+        std::vector<int> dist(nodes, INT_MAX);
         dist[start] = 0;
-        std ::vector<int> relaxed_nodes = {start};
-        int t = _threads;
-        while (relaxed_nodes.size())
+        std::vector<int> relaxed_nodes = {start};
+        std::vector<std::atomic<bool>> in_queue(nodes);
+        for (int i = 0; i < nodes; ++i)
         {
-            
+            in_queue[i] = false;
+        }
+        in_queue[start] = true;
+
+        int t = _threads;
+
+        while (!relaxed_nodes.empty())
+        {
             int sz = relaxed_nodes.size();
             int block_size = (sz + t - 1) / t;
-            std ::vector<std ::future<void>> futures;
-            std ::vector<int> new_relaxed_node;
+
+            std::vector<std::future<void>> futures;
+            std::vector<int> new_relaxed_node;
             std::mutex mtx;
+
             for (int st = 0; st < sz; st += block_size)
             {
                 int end = std::min(st + block_size, sz);
                 futures.emplace_back(pool.submit([&, st, end]()
-                {
-                    std ::vector<int> local_relaxed_nodes;
-                    local_relaxed_nodes.reserve(end-st);
-                    for(int i=st; i<end; i++){
-                        int u=relaxed_nodes[i];
-                        int d=dist[u];
-                        for(auto &[v,w]:Adj[u]){
-                            int new_dist=d+w;
-                            int old_dist=dist[v];
-                            while(old_dist>new_dist){
-                                if (__atomic_compare_exchange_n(&dist[v], &old_dist, new_dist, false,__ATOMIC_RELAXED, __ATOMIC_RELAXED))
-                                {
+                                                 {
+                std::vector<int> local_relaxed_nodes;
+
+                local_relaxed_nodes.reserve(end - st); 
+                
+                for(int i = st; i < end; i++){
+                    int u = relaxed_nodes[i];
+                    
+                    in_queue[u].store(false, std::memory_order_relaxed);
+                    
+                    int d = dist[u];
+                    for(auto &[v, w] : Adj[u]){
+                        int new_dist = d + w;
+                        int old_dist = dist[v];
+                        
+                        while(old_dist > new_dist){
+                            if (__atomic_compare_exchange_n(&dist[v], &old_dist, new_dist, false, __ATOMIC_RELAXED, __ATOMIC_RELAXED))
+                            {
+                               
+                                if (!in_queue[v].exchange(true, std::memory_order_relaxed)) {
                                     local_relaxed_nodes.push_back(v);
-                                    break;
                                 }
+                                break;
                             }
-
                         }
-
-                    } 
-                    if(local_relaxed_nodes.size()){
-                        std :: lock_guard<std :: mutex> lock(mtx);
-                        new_relaxed_node.insert(new_relaxed_node.end(),local_relaxed_nodes.begin(),local_relaxed_nodes.end());
                     }
-                }));
+                } 
+                if(!local_relaxed_nodes.empty()){
+                    std::lock_guard<std::mutex> lock(mtx);
+                    new_relaxed_node.insert(new_relaxed_node.end(), local_relaxed_nodes.begin(), local_relaxed_nodes.end());
+                } }));
             }
-            for(auto &fut:futures)fut.get();
+            for (auto &fut : futures)
+                fut.get();
 
-            relaxed_nodes =std:: move( new_relaxed_node);
+            relaxed_nodes = std::move(new_relaxed_node);
         }
+
         for (auto &x : dist)
         {
             if (x == INT_MAX)
@@ -118,7 +135,7 @@ namespace Dijkstra
         start = std ::chrono::high_resolution_clock::now();
         std ::vector<int> dist_Para = Para_Di(0, nodes, pool, _threads);
         end = std ::chrono::high_resolution_clock::now();
-        
+
         auto Para_time = std ::chrono ::duration<double>(end - start).count();
 
         std ::cout << "Dijkstra :-\n";
